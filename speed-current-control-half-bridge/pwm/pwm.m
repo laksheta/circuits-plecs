ecd2e4521905c365b61a290182232a670755e32a
Laksheta <c10122016@gmail.com>
2025-11-15 03:33:07 +0700

    dc_mot - pwm.m - tune parameter

--- speed-current-control-half-bridge/pwm/pwm.m
+++ speed-current-control-half-bridge/pwm/pwm.m
@@ -16,8 +16,8 @@ vf 		    = Rf*i_f
 
 
 
-J			= 1.54e-5 %Rotor Inertia and unit conversion
-F			= 9.549e-6 %Damping Factor and unit conversion
+J			= 2.118e-4%1.54e-5 %Rotor Inertia and unit conversion
+F			= 1.909e-4%9.549e-6 %Damping Factor and unit conversion
 Laf         = 0.0187 %Torque Sensitivityu
 
 ia_rated    = 8.20 %Rated Current
