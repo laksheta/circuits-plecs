4971ec7061fafd8613acd843b758c358db7a8c79
Laksheta <c10122016@gmail.com>
2025-11-15 02:37:19 +0700

    dc_mot - pwm - done testing current control

--- speed-current-control-half-bridge/pwm/pwm.m
+++ speed-current-control-half-bridge/pwm/pwm.m
@@ -1,6 +1,18 @@
-%% Model Parameters
+%% Model Parameters %%
 
 Vin     = 30
 fsw     = 10e3
 
 ts      = 10%1/fsw * 0.5
+
+Ra 		    = 0.43 %Terminal Resistance
+La			= 0.9e-3 %Terminal Inductance
+
+%% Control Parameters %%
+
+%% Current Control
+f_bw_c    = fsw/10
+w_bw_c    = 2*pi*f_bw_c
+
+Kp_c    = w_bw_c * La
+Ki_c    = w_bw_c * Ra
