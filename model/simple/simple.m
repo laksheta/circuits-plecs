579cc6f566b65f56db733006da9064a5018c7f9f
Laksheta <krishnalaksheta@gmail.com>
2025-11-24 09:08:13 +0700

    model_simple - plecs - add power calculation

--- model/simple/simple.m
+++ model/simple/simple.m
@@ -1,11 +1,13 @@
+plecs('clc')
 fsw	    = 10e3
 
 V1		= 780
 V2 	    = 780
 
 P       = 10e3
-D       = 0.25       %! Normalized phase shift ratio
+D       = 0.5       %! Normalized phase shift ratio
 L		= (V1*V2)*D*(1-D)/(2*fsw*P)
 
-deg	    = D*(90/0.5)
+d       = 0.5
+deg	    = d*(90/0.5)
 phi	    = (deg/360)*(1/fsw)
