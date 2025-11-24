5c54c7b98ca82288c570abe4f8ea8335e5e0b6d1
Laksheta <krishnalaksheta@gmail.com>
2025-11-24 08:23:39 +0700

    model_simple - m - done adding L calculation

--- model/simple/simple.m
+++ model/simple/simple.m
@@ -4,8 +4,8 @@ V1		= 780
 V2 	    = 780
 
 P       = 10e3
-D       = 0.5       %! Normalized phase shift ratio
+D       = 0.25       %! Normalized phase shift ratio
 L		= (V1*V2)*D*(1-D)/(2*fsw*P)
 
-deg	    = 30;
-phi	    = (deg/360)*(1/fsw);
+deg	    = D*(90/0.5)
+phi	    = (deg/360)*(1/fsw)
