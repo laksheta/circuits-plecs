1ce800b58b57175deb556bcd9507e4eab43e2fcb
Laksheta <krishnalaksheta@gmail.com>
2025-11-26 15:47:30 +0700

    model_simple - plecs - tune R_load

--- model/simple/simple.m
+++ model/simple/simple.m
@@ -6,7 +6,6 @@ V1		= 780
 V2 	    = 780
 C       = 1e-6
 
-R_load  = 100
 P       = 10e3
 D       = 0.5       %! Normalized phase shift ratio
 L		= (V1*V2)*D*(1-D)/(2*fsw*P)
@@ -14,3 +13,6 @@ L		= (V1*V2)*D*(1-D)/(2*fsw*P)
 d       = 0.5
 deg	    = d*(90/0.5)
 phi	    = (deg/360)*(1/fsw)
+
+I       = V1*D*(1-D)/(2*fsw*L)
+R_load  = V1/I
