111f4466029c7c7a3ad36bbfb89b0e0cd3272051
Laksheta <krishnalaksheta@gmail.com>
2025-11-27 07:33:30 +0700

    model_control - m - done troubleshooting - recall that 1 -> 360 deg in modulator

--- model/control/control.m
+++ model/control/control.m
@@ -1,7 +1,7 @@
 plecs('clc')
 fsw	    = 10e3
 
-ts      = 1%100*1/fsw
+ts      = 10*1/fsw
 V1		= 780
 V2 	    = 780
 C       = 1e-6
@@ -10,7 +10,7 @@ P       = 10e3
 D       = 0.5       %! Normalized phase shift ratio
 L		= (V1*V2)*D*(1-D)/(2*fsw*P)
 
-d       = 0.25
+d       = 0.25 * 0.5
 deg	    = d*(90/0.5)
 phi	    = (deg/360)*(1/fsw)
 
