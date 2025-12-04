9c75277794e4d2a8c04df24e59f2dbc40cd5883c
Laksheta <krishnalaksheta@gmail.com>
2025-12-04 11:15:13 +0700

    model_model - setup

---
+++ model/model/model.m
@@ -0,0 +1,23 @@
+plecs('clc')
+fsw	    = 10e3
+
+ts      = 10*1/fsw
+V1		= 780
+V2 	    = 780
+C       = 1e-6
+
+P       = 10e3
+D       = 0.5       %! Normalized phase shift ratio
+L		= (V1*V2)*D*(1-D)/(2*fsw*P)
+
+d       = 0.5
+d       = d*0.5
+deg	    = d*(90/0.5)
+phi	    = (deg/360)*(1/fsw)
+
+I       = V1*D*(1-D)/(2*fsw*L)  %! Current calculation
+R_load  = V1/I
+
+%* PI Controller
+Kp      = 0.00422
+Ki      = 30.7
