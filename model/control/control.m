771f5a46096c861ca5f21958a2c6f20147f591b0
Laksheta <krishnalaksheta@gmail.com>
2025-12-05 15:27:54 +0700

    model_control - add paramter

--- model/control/control.m
+++ model/control/control.m
@@ -1,10 +1,11 @@
 plecs('clc')
 fsw	    = 10e3
 
-ts      = 10*1/fsw
+ts      = 100*1/fsw
 V1		= 780
 V2 	    = 780
-C       = 1e-6
+V_out   = V1
+C       = 1e-3%1e-6
 
 P       = 10e3
 D       = 0.5       %! Normalized phase shift ratio
@@ -19,5 +20,12 @@ I       = V1*D*(1-D)/(2*fsw*L)  %! Current calculation
 R_load  = V1/I
 
 %* PI Controller
-Kp      = 0.00422
-Ki      = 30.7
+
+Gdc     = ((R_load)/(2*V_out))*((V1*V2*(1-2*d)/(2*fsw*L)))
+tau     = (C*R_load)/2
+
+f_bw    = 0.1*fsw
+omega_c = 2*pi*f_bw
+
+Kp      = (tau*omega_c)/Gdc
+Ki      = Kp/tau
