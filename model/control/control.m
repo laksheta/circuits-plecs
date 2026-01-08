29ff150aeba5fba5bb15ae02cf126ef2dfdba78e
Laksheta <krishnalaksheta@gmail.com>
2026-01-08 11:15:48 +0700

    control - m - update code

--- model/control/control.m
+++ model/control/control.m
@@ -7,10 +7,11 @@ V2 	    = 780
 V_out   = V1
 C       = 1e-5%1e-6
 
-P_set   = 5e3
-P       = 10e3%P_set
+P_c     = 5e3
+P_set   = 10e3
+P_design       = 15e3
 D       = 0.5       %! Normalized phase shift ratio
-L		= (V1*V2)*D*(1-D)/(2*fsw*P)
+L		= (V1*V2)*D*(1-D)/(2*fsw*P_design)
 
 d       = 0.5
 d       = d*0.5
@@ -18,15 +19,15 @@ deg	    = d*(90/0.5)
 phi	    = (deg/360)*(1/fsw)
 
 I       = V1*D*(1-D)/(2*fsw*L)  %! Current calculation
-R_load_c  = V1/I
-R_load  = 2*(V_out^2) / P_set
+R_load_c  = (V_out^2) / P_c 
+R_load  = (V_out^2) / P_set
 
 %* PI Controller
 
 Gdc     = ((R_load_c)/(2*V_out))*((V1*V2*(1-2*d)/(2*fsw*L)))
 tau     = (C*R_load_c)/2
 
-f_bw    = 0.1*fsw
+f_bw    = 0.05*fsw
 omega_c = 2*pi*f_bw
 
 Kp      = (tau*omega_c)/Gdc
